081fd6fa9e3e97782cf0e5cee1e62715b5886026
Boris Belmar <borisbelmarm@gmail.com>
2024-11-11 10:50:46 -0300

    First commit

---
+++ database.types.ts
@@ -0,0 +1,234 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export type Database = {
+  public: {
+    Tables: {
+      hits: {
+        Row: {
+          created_at: string
+          id: number
+          link: string | null
+        }
+        Insert: {
+          created_at?: string
+          id?: number
+          link?: string | null
+        }
+        Update: {
+          created_at?: string
+          id?: number
+          link?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "hits_link_fkey"
+            columns: ["link"]
+            isOneToOne: false
+            referencedRelation: "links"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      links: {
+        Row: {
+          created_at: string
+          id: string
+          name: string
+          url: string
+          user: string | null
+        }
+        Insert: {
+          created_at?: string
+          id: string
+          name: string
+          url: string
+          user?: string | null
+        }
+        Update: {
+          created_at?: string
+          id?: string
+          name?: string
+          url?: string
+          user?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "links_user_fkey"
+            columns: ["user"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      users: {
+        Row: {
+          created_at: string
+          email: string
+          id: string
+          password: string
+        }
+        Insert: {
+          created_at?: string
+          email: string
+          id?: string
+          password: string
+        }
+        Update: {
+          created_at?: string
+          email?: string
+          id?: string
+          password?: string
+        }
+        Relationships: []
+      }
+    }
+    Views: {
+      hits_count: {
+        Row: {
+          count: number | null
+          link: string | null
+          name: string | null
+          url: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "hits_link_fkey"
+            columns: ["link"]
+            isOneToOne: false
+            referencedRelation: "links"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+    }
+    Functions: {
+      count_hits_with_url: {
+        Args: Record<PropertyKey, never>
+        Returns: {
+          link: string
+          url: string
+          hit_count: number
+        }[]
+      }
+      hits_count: {
+        Args: Record<PropertyKey, never>
+        Returns: {
+          link: string
+          name: string
+          url: string
+          count: number
+        }[]
+      }
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
+
+type PublicSchema = Database[Extract<keyof Database, "public">]
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
+        PublicSchema["Views"])
+    ? (PublicSchema["Tables"] &
+        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+        Row: infer R
+      }
+      ? R
+      : never
+    : never
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Insert: infer I
+      }
+      ? I
+      : never
+    : never
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Update: infer U
+      }
+      ? U
+      : never
+    : never
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof PublicSchema["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never,
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
+    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+    : never
+
+export type CompositeTypes<
+  PublicCompositeTypeNameOrOptions extends
+    | keyof PublicSchema["CompositeTypes"]
+    | { schema: keyof Database },
+  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    : never = never,
+> = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
+    ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+    : never
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,54 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Database } from '../database.types'
+import { createClient } from '@supabase/supabase-js'
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request, env): Promise<Response> {
+		const linkHash = request.url.split('/').pop()
+		if (!linkHash) {
+			return new Response('Invalid URL', { status: 400 })
+		}
+		const supabase = createClient<Database> (env.SUPABASE_URL, env.SUPABASE_KEY)
+		if (linkHash === 'download-csv') {
+			const { data, error } = await supabase.from('hits_count')
+				.select('link, name, url, count')
+				.order('count', { ascending: false })
+			if (error) {
+				console.error('Error fetching hits', error)
+				return new Response('Error fetching hits', { status: 500 })
+			}
+			const csv = data.map(hit => `${hit.link},${hit.name},${hit.url},${hit.count}\n`).join('')
+			const csvHeader = 'Link,Name,URL,Count\n'
+			return new Response(`${csvHeader}${csv}`, {
+				headers: {
+					'Content-Type': 'text/csv',
+					'Content-Disposition': `attachment; filename="hits-${Date.now()}.csv"`,
+				}
+			})	
+		}
+		if (linkHash.length !== 10 || !/^[a-zA-Z0-9]+$/.test(linkHash)) {
+			return new Response('Invalid URL', { status: 400 })
+		}
+
+		const { data, error } = await supabase.from('links').select('url').eq('id', linkHash)
+
+		if (error) {
+			console.error('Error fetching link', error)
+			return new Response('Error fetching link', { status: 500 })
+		}
+		if (!data || data.length === 0) {
+			return new Response('Link not found', { status: 404 })
+		}
+		const destinationURL = data[0].url
+		const statusCode = 301;
+
+		await supabase.from('hits').insert({ link: linkHash })
+		const headers = new Headers();
+		headers.set('Cache-Control', 'no-store');
+		headers.set('Location', destinationURL);
+
+		return new Response(null, {
+			status: statusCode,
+			headers
+		});
 	},
 } satisfies ExportedHandler<Env>;
